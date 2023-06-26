308833177aff462d999eb090f328ced82f723330
matigobbi <matigobbi@gmail.com>
2023-06-26 18:10:00 +0200

    .

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,11 +9,11 @@ function Home() {
       <section>
         <h1>жNavarré-Lopez  </h1>
         <p>
-        Guille Navarre Lopez is a graphic designer and illustrator based in Berlin. His works are focused on art direction, branding, UI, editorial design and motion graphics.
-        Guille's designs combine the audacity and rebelliousness of a young visual creator with the astuteness and effectiveness of an experienced communicator.
+          Guille Navarre Lopez is a graphic designer and illustrator based in Berlin. His works are focused on art direction, branding, UI, editorial design and motion graphics.
+          Guille's designs combine the audacity and rebelliousness of a young visual creator with the astuteness and effectiveness of an experienced communicator.
         </p>
       </section>
-      
+
 
     </div>
     
