9cd3c093596cce74d98d4ce8190332109033c0c3
matigobbi <matigobbi@gmail.com>
2023-07-17 00:16:07 +0200

    poster designs added

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,13 +9,21 @@ function Home() {
           <h1 className="moving-name">GuilleжNavarré-Lopez GuilleжNavarré-Lopez GuilleжNavarré-Lopez GuilleжNavarré-Lopez GuilleжNavarré-Lopez GuilleжNavarré-Lopez</h1>
         </div>
         <div className="white-line"></div>
-        <p>
-          Guille Navarre Lopez is a graphic designer and illustrator based in
-          Berlin. His works are focused on art direction, branding, UI,
-          editorial design and motion graphics. Guille's designs combine the
-          audacity and rebelliousness of a young visual creator with the
-          astuteness and effectiveness of an experienced communicator.
-        </p>
+        <div className="description-container">
+          <p>
+            Guille Navarre Lopez is a graphic designer and illustrator based in
+            Berlin. His works are focused on art direction, branding, UI,
+            editorial design and motion graphics. Guille's designs combine the
+            audacity and rebelliousness of a young visual creator with the
+            astuteness and effectiveness of an experienced communicator.
+          </p>
+          <div> </div>
+        </div>
+        <div className="posterdesign-container">
+          <img src="../public/GREECE-IG-SQUARE.png" alt="poster1" />
+          <img src="../public/EGYPT-IG-SQUARE.png" alt="poster1" />
+          <img src="../public/TBILISI-IG-SQUARE.png" alt="poster1" />
+        </div>
       </section>
     </div>
   );
