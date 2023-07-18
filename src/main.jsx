e51299a4db2f1f73aa5ca08067847986bb9251b3
Carolina Acuna <carolina.acuna90@gmail.com>
2023-07-18 17:20:34 +0200

    rutas ordenadas

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./Home.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Routes, Route, Link } from "react-router-dom"; 
+import { Routes, Route, Link } from "react-router-dom";
 import Nav from "./components/Nav/Nav.jsx";
 import { Work } from "./components/Work";
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<About />} />
-        <Route path="/about" element={<Contact />} />
-        <Route path="/work" element={<Work />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/work' element={<Work />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </Router>
   </React.StrictMode>
