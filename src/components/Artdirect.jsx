d22350981a9dba846496dd4e86fdddf0d57ae2fb
matigobbi <matigobbi@gmail.com>
2023-07-26 19:57:10 +0200

    art direct

--- src/components/Artdirect.jsx
+++ src/components/Artdirect.jsx
@@ -10,6 +10,19 @@ export const Artdirect = () => {
         <img src="images/artdirect-fondo.jpeg" alt="" />
         <div className="borderless">Borderless</div>
       </div>
+      <div className="images-container">
+        <div><img src="images/artdirect-1.png" alt="" /></div>
+        <div><img src="images/artdirect-2.png" alt="" /></div>
+        <div><img src="images/artdirect-3.png" alt="" /></div>
+        <div><img src="images/artdirect-4.png" alt="" /></div>
+        <div><img src="images/artdirect-5.png" alt="" /></div>
+        <div><img src="images/artdirect-6.png" alt="" /></div>
+        <div><img src="images/artdirect-7.png" alt="" /></div>
+        <div><img src="images/artdirect-8.png" alt="" /></div>
+        <div><img src="images/artdirect-9.png" alt="" /></div>
+        <div><img src="images/artdirect-10.png" alt="" /></div>
+        <div><img src="images/artdirect-11.png" alt="" /></div>
+      </div>
     </div>
   )
 }
